87d1f675c1d58f0cdab2a2b7823f85d9ea68b5bf
a-anuj <anujamirthalingam5@gmail.com>
2024-12-19 21:22:09 +0530

    created another view

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World")
+
+def products(request):
+    all_products = ["Apple","Mango","Banana","Cherry","Kiwi","Jack Fruit"]
+    return HttpResponse(products)
