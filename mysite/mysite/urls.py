e9d947196ff50ec290d7718cd83e6b952df08372
a-anuj <anujamirthalingam5@gmail.com>
2024-12-19 21:14:00 +0530

    added local urls folder in myapp directory

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from myapp import views
-
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index),
+    path('myapp/',include('myapp.urls')),
 ]
