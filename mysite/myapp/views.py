e3e28f2b25841001cdbc0cdf51a9c59b4aab687c
a-anuj <anujamirthalingam5@gmail.com>
2024-12-22 08:23:53 +0530

    getting products inside view

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World")
 
 def products(request):
-    all_products = ["Apple","Mango","Banana","Cherry","Kiwi","Jack Fruit"]
-    return HttpResponse(products)
+    all_products = Product.objects.all()
+    return HttpResponse(all_products) 
