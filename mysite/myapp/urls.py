41e672100ccc43bc91b17c6db8fc97a70ea1c5a6
a-anuj <anujamirthalingam5@gmail.com>
2024-12-22 09:14:42 +0530

    namespacing the urls

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = "myapp"
 urlpatterns = [
     path('',views.index),
     path('products/',views.products),
-    path('products/<int:id>/',views.product_details),
+    path('products/<int:id>/',views.product_details,name="product_details"),
 ]
