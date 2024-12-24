baa9a3015c1c009ccb5638e91c50610f295d20fd
a-anuj <anujamirthalingam5@gmail.com>
2024-12-24 18:42:50 +0530

    frontend for update part

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -30,4 +30,8 @@ def add_product(request):
     return render(request,'myapp/addproduct.html')
 
 def update_product(request,id):
-    return render(request,'myapp/updateproduct.html')
+    product = Product.objects.get(id=id)
+    context = {
+        "product" : product
+    }
+    return render(request,'myapp/updateproduct.html',context)
