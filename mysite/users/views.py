7ec0625eb762318effaf1b9473786847be87b116
a-anuj <anujamirthalingam5@gmail.com>
2024-12-28 21:04:33 +0530

    added a createprofile page

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -19,3 +19,6 @@ def register(request):
 @login_required
 def profile(request):
     return render(request,'users/profile.html')
+
+def create_profile(request):
+    return render(request,'users/createprofile.html')
