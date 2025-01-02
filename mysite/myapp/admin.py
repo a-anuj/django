bc48437fc4f91c5eea132bb9357f3414d2a63134
a-anuj <anujamirthalingam5@gmail.com>
2025-01-02 18:08:16 +0530

    adding custom fields

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from .models import Product
 # Register your models here.
 
-admin.site.register(Product)
+
 admin.site.site_header = "Buy and Sell Website"
 admin.site.site_title = "QuickCart"
 admin.site.index_title = "Manage QuickCart"
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','price','desc')
+
+
+admin.site.register(Product,ProductAdmin)
