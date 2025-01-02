f1bf089e0fe1e47699113904931a842b3f6e110b
a-anuj <anujamirthalingam5@gmail.com>
2025-01-02 18:04:33 +0530

    modifying admin headers

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -3,3 +3,6 @@ from .models import Product
 # Register your models here.
 
 admin.site.register(Product)
+admin.site.site_header = "Buy and Sell Website"
+admin.site.site_title = "QuickCart"
+admin.site.index_title = "Manage QuickCart"
